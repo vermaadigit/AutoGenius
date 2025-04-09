a0966128a4dbcee10a4fb8e805a5a1bfadb8e69a
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-10 00:28:10 +0530

    Header Updated

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  SignIn,
   SignInButton,
   SignUpButton,
   SignedIn,
@@ -8,6 +9,8 @@ import {
 } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
 
 const Header = ({ isAdminPage = false }) => {
   const isAdmin = false;
@@ -15,7 +18,7 @@ const Header = ({ isAdminPage = false }) => {
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="mx-auto px-4 py-4 flex items-center justify-between">
-        <Link href={isAdminPage ? "/admin" : "/"}>
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
           <Image
             src={"/Main_Logo.png"}
             alt="Auto Genius Logo"
@@ -27,6 +30,58 @@ const Header = ({ isAdminPage = false }) => {
             <span className="text-xs font-extralight">admin</span>
           )}
         </Link>
+
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href={"/"}>
+              <Button variant={"outline"} className="flex items-center gap-2">
+                <ArrowLeft size={18} />
+                <span className="hidden md:inline">Back To App</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link href={"/saved-cars"}>
+                <Button>
+                  <Heart size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+
+              {!isAdmin ? (
+                <Link href={"/reservations"}>
+                  <Button variant={"outline"}>
+                    <CarFront size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href={"/reservations"}>
+                  <Button variant={"outline"}>
+                    <Layout size={18} />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/">
+              <Button variant={"outline"}>Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
       </nav>
     </header>
   );
