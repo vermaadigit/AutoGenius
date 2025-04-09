51b991f753aef19fbb8dd507d51cf57e83213a52
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-09 15:00:13 +0530

    Auth Functionality Added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,16 +12,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <main className="min-h-screen">{children}</main>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
 
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600 ">
-            <p>Made with 💖 by Aditya Verma</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600 ">
+              <p>Made with 💖 by Aditya Verma</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
