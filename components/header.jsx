b37b8327ac6f2958840d5c80773647d4012a4aeb
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-09 15:00:54 +0530

    Signout, SignIn and UserButton Endpoint

---
+++ components/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
