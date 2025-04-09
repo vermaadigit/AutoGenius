2d943e51db015e1b7c4d7a51c82f79fedd76d407
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-09 15:22:08 +0530

    Protected Routes Added

--- middleware.js
+++ middleware.js
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/admin(.*)",
+  "/saved-cars(.*)",
+  "/reservations(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
