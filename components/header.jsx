5f0f0e02b13de2aa3ff435138d26e7961563a5e3
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-09 21:10:13 +0530

    Header Logo Updated

--- components/header.jsx
+++ components/header.jsx
@@ -6,18 +6,29 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
 
-const Header = () => {
+const Header = ({ isAdminPage = false }) => {
+  const isAdmin = false;
+
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"}>
+          <Image
+            src={"/Main_Logo.png"}
+            alt="Auto Genius Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">admin</span>
+          )}
+        </Link>
+      </nav>
+    </header>
   );
 };
 
