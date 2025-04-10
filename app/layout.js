15bf9ee322242d52452589e08db666d01dd8f3d5
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-11 02:07:43 +0530

    Toaster Component @sonner

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600 ">
