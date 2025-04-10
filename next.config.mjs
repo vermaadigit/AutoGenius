1c3e763590622620c52d77b9202e1b2677f5a112
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-10 11:52:34 +0530

    Create.xyz IFrame Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "content-security-policy",
+            value: "frame-src 'self' https://autogenius.created.app",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
