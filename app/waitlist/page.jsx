7d3a6773199c0b2154df68d8b358beccc617586d
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-10 11:53:09 +0530

    Waitlist Page @create.xyz

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const WaitlistPage = () => {
+  return (
+    <div className="mt-20 fixed w-full">
+      <div className="aspect-video w-full">
+        <iframe
+          style={{ width: "100%" }}
+          height="569"
+          src="https://www.create.xyz/app/214c9101-24ff-4a4c-b420-2de5f1087698"
+          title="WaitingListLandingPage"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default WaitlistPage;
