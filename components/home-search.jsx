af5111e8696e94b40a8aa719c80f1c591cfa2144
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-11 09:41:51 +0530

    Home Search Component Added

--- components/home-search.jsx
+++ components/home-search.jsx
@@ -2,12 +2,77 @@
 
 import React, { useState } from "react";
 import { Input } from "./ui/input";
-import { Camera } from "lucide-react";
+import { Camera, Upload } from "lucide-react";
+import { Button } from "./ui/button";
+import { useDropzone } from "react-dropzone";
+import { Toaster } from "./ui/sonner";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const HomeSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [isImageSearchActive, setIsImageSearchActive] = useState(false);
-  const handleTextSubmit = () => {};
+  const [imagePreview, setImagePreview] = useState("");
+  const [searchImage, setSearchImage] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const router = useRouter();
+
+  const handleTextSubmit = (e) => {
+    e.preventDefault();
+    if (!searchTerm.trim()) {
+      toast.error("Please enter a search term");
+      return;
+    }
+
+    router.push(`/cars?search=${encodeURIComponent(searchTerm)}`);
+  };
+
+  const handleImageSearch = async (e) => {
+    e.preventDefault();
+    if (!searchImage) {
+      toast.error("Please upload an image first");
+      return;
+    }
+
+    //add ai logic
+  };
+
+  const onDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+
+    if (file) {
+      if (file.size > 5 * 1024 * 1024) {
+        toast.error("Image size must be less than 5MB");
+        return;
+      }
+
+      setIsUploading(true);
+      setSearchImage(file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+        setIsUploading(false);
+        toast.success("Image Uploaded Successfully");
+      };
+
+      reader.onerror = () => {
+        setIsUploading(false);
+        toast.error("Failed to read the Image");
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+  const { getRootProps, getInputProps, isDragActive, isDragReject } =
+    useDropzone({
+      onDrop,
+      accept: {
+        "image/*": [".jpg", ".jpeg", ".png", ".webp"],
+      },
+      maxFiles: 1,
+    });
 
   return (
     <div>
@@ -34,8 +99,66 @@ const HomeSearch = () => {
               }}
             />
           </div>
+          <Button type="submit" className={"absolute right-2 rounded-full"}>
+            Search
+          </Button>
         </div>
       </form>
+
+      {isImageSearchActive && (
+        <div className="mt-4">
+          <form onSubmit={handleImageSearch}>
+            <div className="border-2 border-dashed border-gray-300 rounded-3xl p-6 text-center">
+              {imagePreview ? (
+                <div className="flex flex-col items-center">
+                  <img
+                    src={imagePreview}
+                    alt="Car preview"
+                    className="h-40 object-contain mb-4"
+                  />
+                  <Button
+                    variant={"outline"}
+                    onClick={() => {
+                      setSearchImage(null);
+                      setImagePreview("");
+                      toast.info("Image Removed");
+                    }}
+                  >
+                    Remove Image
+                  </Button>
+                </div>
+              ) : (
+                <div {...getRootProps()} className="cursor-pointer">
+                  <input {...getInputProps()} />
+                  <div className="flex flex-col items-center">
+                    <Upload className="h-12 w-12 text-gray-400 mb-2" />
+                    <p className="text-gray-500 mb-2">
+                      {isDragActive && isDragReject
+                        ? "Leave the file here to upload"
+                        : "Drag & Drop a car Image or click to select"}
+                    </p>
+                    {isDragReject && (
+                      <p className="text-red-500 mb-2">Invalid Image Type</p>
+                    )}
+                    <p className="text-gray-400 text-sm">
+                      Supports : JPG, PNG (max 5MB)
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+            {imagePreview && (
+              <Button
+                type="submit"
+                className={"w-full mt-2"}
+                disabled={isUploading}
+              >
+                {isUploading ? "Uploading..." : "Search with this Image"}
+              </Button>
+            )}
+          </form>
+        </div>
+      )}
     </div>
   );
 };
