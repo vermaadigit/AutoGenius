ff8a43e3c9a800e3a175b425f9b064895dea9003
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-16 16:32:33 +0530

    Server Component HMR Cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xakzfyjyirjdfecvnfps.supabase.co",
+      },
+    ],
+  },
+
   async headers() {
     return [
       {
