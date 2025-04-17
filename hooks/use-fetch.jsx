63e557f4d459695288b46e24dfc0adbb5ffbf9f5
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-17 17:37:05 +0530

    CallBack Function to call API

--- hooks/use-fetch.jsx
+++ hooks/use-fetch.jsx
@@ -1,1 +1,29 @@
-const useFetch = (cb) => {};
+import { useFormState } from "react-dom";
+import { useState } from "react";
+import { toast } from "sonner";
+
+const useFetch = (cb) => {
+  const [data, setData] = useFormState(undefined);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fn = async (...args) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(...args);
+      setData(response);
+      setError(null);
+    } catch (error) {
+      setError(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn, setData };
+};
+
+export default useFetch;
