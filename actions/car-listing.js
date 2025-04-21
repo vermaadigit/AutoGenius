6d370788cbbf61b02ca511edcd9df481ca216483
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-21 22:51:54 +0530

    Toggle Save Car EP

--- actions/car-listing.js
+++ actions/car-listing.js
@@ -178,3 +178,15 @@ export async function getCars({
     throw new Error("Error fetching cars:" + error.message);
   }
 }
+export async function toggleSavedCar(carId) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) throw new Error("User not found");
+  } catch (error) {}
+}
