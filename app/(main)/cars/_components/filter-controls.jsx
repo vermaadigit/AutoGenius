4b955ca5ae13016e39d4fda3652edb2f6b9613d2
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-22 15:11:28 +0530

    Car Filter Control

---
+++ app/(main)/cars/_components/filter-controls.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const CarFilterControls = ({
+  filters,
+  currentFilters,
+  onFilterChange,
+  onClearFilter,
+}) => {
+  return <div>CarFilterControls</div>;
+};
+
+export default CarFilterControls;
