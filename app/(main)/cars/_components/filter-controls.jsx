60b4d42fb824395d4d88bb68fce22eab24e19b8f
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-22 15:57:37 +0530

    Filter Control Flow

--- app/(main)/cars/_components/filter-controls.jsx
+++ app/(main)/cars/_components/filter-controls.jsx
@@ -8,6 +8,42 @@ const CarFilterControls = ({
   onFilterChange,
   onClearFilter,
 }) => {
+  const { make, bodyType, fuelType, transmission, priceRange } = currentFilters;
+
+  const filterSections = [
+    {
+      id: "make",
+      title: "Make",
+      options: filters.makes.map((make) => ({ value: make, label: make })),
+      currentValue: make,
+      onChange: (value) => onFilterChange("make", value),
+    },
+    {
+      id: "bodyType",
+      title: "Body Type",
+      options: filters.bodyTypes.map((type) => ({ value: type, label: type })),
+      currentValue: bodyType,
+      onChange: (value) => onFilterChange("bodyType", value),
+    },
+    {
+      id: "fuelType",
+      title: "Fuel Type",
+      options: filters.fuelTypes.map((type) => ({ value: type, label: type })),
+      currentValue: fuelType,
+      onChange: (value) => onFilterChange("fuelType", value),
+    },
+    {
+      id: "transmission",
+      title: "Transmission",
+      options: filters.transmissions.map((type) => ({
+        value: type,
+        label: type,
+      })),
+      currentValue: transmission,
+      onChange: (value) => onFilterChange("transmission", value),
+    },
+  ];
+
   return <div>CarFilterControls</div>;
 };
 
