5c953b1a5105014867c488b31b0bb2f59e34de0a
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-04-24 22:48:34 +0530

    Admin Revalidation Path

--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import {
-  SignIn,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-import Link from "next/link";
-import Image from "next/image";
 import { Button } from "./ui/button";
-import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+import { Heart, CarFront, Layout, ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { checkUser } from "@/lib/checkUser";
+import Image from "next/image";
 
 const Header = async ({ isAdminPage = false }) => {
   const user = await checkUser();
-
   const isAdmin = user?.role === "ADMIN";
 
   return (
@@ -24,7 +16,7 @@ const Header = async ({ isAdminPage = false }) => {
         <Link href={isAdminPage ? "/admin" : "/"} className="flex">
           <Image
             src={"/Main_Logo.png"}
-            alt="Auto Genius Logo"
+            alt="AutoGenius Logo"
             width={200}
             height={60}
             className="h-12 w-auto object-contain"
@@ -34,33 +26,39 @@ const Header = async ({ isAdminPage = false }) => {
           )}
         </Link>
 
+        {/* Action Buttons */}
         <div className="flex items-center space-x-4">
           {isAdminPage ? (
-            <Link href={"/"}>
-              <Button variant={"outline"} className="flex items-center gap-2">
-                <ArrowLeft size={18} />
-                <span className="hidden md:inline">Back To App</span>
-              </Button>
-            </Link>
-          ) : (
-            <SignedIn>
-              <Link href={"/saved-cars"}>
-                <Button>
-                  <Heart size={18} />
-                  <span className="hidden md:inline">Saved Cars</span>
+            <>
+              <Link href="/">
+                <Button variant="outline" className="flex items-center gap-2">
+                  <ArrowLeft size={18} />
+                  <span>Back to App</span>
                 </Button>
               </Link>
-
-              {!isAdmin ? (
-                <Link href={"/reservations"}>
-                  <Button variant={"outline"}>
+            </>
+          ) : (
+            <SignedIn>
+              {!isAdmin && (
+                <Link
+                  href="/reservations"
+                  className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+                >
+                  <Button variant="outline">
                     <CarFront size={18} />
                     <span className="hidden md:inline">My Reservations</span>
                   </Button>
                 </Link>
-              ) : (
-                <Link href={"/reservations"}>
-                  <Button variant={"outline"}>
+              )}
+              <a href="/saved-cars">
+                <Button className="flex items-center gap-2">
+                  <Heart size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </a>
+              {isAdmin && (
+                <Link href="/admin">
+                  <Button variant="outline" className="flex items-center gap-2">
                     <Layout size={18} />
                     <span className="hidden md:inline">Admin Portal</span>
                   </Button>
@@ -70,9 +68,11 @@ const Header = async ({ isAdminPage = false }) => {
           )}
 
           <SignedOut>
-            <SignInButton forceRedirectUrl="/">
-              <Button variant={"outline"}>Login</Button>
-            </SignInButton>
+            {!isAdminPage && (
+              <SignInButton forceRedirectUrl="/">
+                <Button variant="outline">Login</Button>
+              </SignInButton>
+            )}
           </SignedOut>
 
           <SignedIn>
